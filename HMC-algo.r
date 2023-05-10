f8ce66332742db54617bdbb5d90907edbead1ce9
Isaiah <114532921+isaiahkatzt@users.noreply.github.com>
2023-05-10 11:39:18 -0700

    Delete HMC-algo.r

--- HMC-algo.r
+++
@@ -1,8 +0,0 @@
-leapfrog <- function(theta, r, epsilon, theta) {
-  r_update <- r + (epsilon / 2) * grad(L*theta)
-  theta_new <- theta + (epsilon * r_update)
-  r_new <- r_update + (epsilon / 2) * grad(L*theta_new)
-}
-
-hamiltonian_mc <- function(theta_0, epsilon, L, scriptL, M) { 
-  }
